306233d83dca97ca0bbbb65d8d374daa112aff6c
Kimgyuilli <rlarbdlf222@gmail.com>
2025-10-21 06:22:17 +0900

    feat: ToastContainer 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,16 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import { ToastContainer } from '@/shared/components/ui/Toast';
 
 /**
  * 메인 App 컴포넌트
  * React Router v7 기반 라우팅 제공
  */
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </>
+  );
 }
